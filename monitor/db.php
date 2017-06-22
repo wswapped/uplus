b05102d679faf02025e9e327a2e59f77aeca259d
Clement <muhirwaclement@gmail.com>
2017-06-22 23:10:14 +0200

    Update db.php

--- monitor/db.php
+++ monitor/db.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL); 
 ini_set('display_errors', 0);
-		$con = mysqli_connect("localhost", "root", "" , "rtgs");
-		$db = mysqli_connect("localhost", "root", "" , "uplus");
+		$con = mysqli_connect("localhost", "clement", "clement123" , "rtgs");
+		$db = mysqli_connect("localhost", "clement", "clement123" , "uplus");
 ?>
