5c6b5d50290c308580d24a320c19ee17b42e9929
Clement <muhirwaclement@gmail.com>
2017-10-18 07:55:31 +0200

    Create db.php

---
+++ api/db.php
@@ -0,0 +1,14 @@
+<?php  
+error_reporting(E_ALL); 
+ini_set('display_errors', 1);
+	$db = new mysqli("localhost", "clement", "clement123" , "uplus");
+	
+	if($db->connect_errno){
+		die('Uplus is currently not available in your country!');
+	}
+	
+	$outCon  = new mysqli("localhost", "clement", "clement123" , "rtgs");
+	if($outCon->connect_errno){
+		die('Sorry we have some problem with the Money Database!');
+	}
+?>
