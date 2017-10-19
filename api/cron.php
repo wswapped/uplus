7a01c4d7291fdb537f6316b16e0a8fae4ee0be9a
Clement <cmuhirwa@users.noreply.github.com>
2017-10-20 00:53:45 +0200

    add the cron

--- api/cron.php
+++ api/cron.php
@@ -1,27 +1,41 @@
 <?php
-$phone 		= '0784848236';
-$sender 	= "CRONNER";
-$message 	= "The cron is just launched";
-	
-
-//CLEAN PHONE
-$phone 	= preg_replace( '/[^0-9]/', '', $phone );
-$phone 	= substr($phone, -10); 
-
-$recipients = '+25'.$phone;
-//$message    = 'Welcome to UPLUS, please use '.$code.' to log into your account.';
-$data = array(
-	"sender"		=>$sender,
-	"recipients"	=>$recipients,
-	"message"		=>$message,
-);
-include 'sms.php';
-if($httpcode == 200)
+if(isset($_GET['action']))
 {
-	echo "yes";
+	$_GET['action']();
 }
-else
-{
-	echo 'System error';
+
+function status(){
+	include 'db.php';
+	$sql= $outCon->query("SELECT sum(amount) todaybalance FROM directtransfers WHERE transaction_date > DATE_SUB(NOW(), INTERVAL 1 DAY)");
+	$sql2= $outCon->query("SELECT count(id) todaytransactions FROM directtransfers WHERE transaction_date > DATE_SUB(NOW(), INTERVAL 1 DAY)");
+	$balancerow = mysqli_fetch_array($sql);
+	$balancerow2 = mysqli_fetch_array($sql2);
+	$balance 		= $balancerow['todaybalance'];
+	$transactions 	= $balancerow2['todaytransactions'];
+	$phone 		= '0784848236';
+	$sender 	= "CRONNER";
+	$message 	= 'Hello Uplus manager, Today we had '.$balance.' Rwf through UPLUS, from '.$transactions .' transactions.';
+		
+
+	//CLEAN PHONE
+	$phone 	= preg_replace( '/[^0-9]/', '', $phone );
+	$phone 	= substr($phone, -10); 
+
+	$recipients = '+25'.$phone;
+	//$message    = 'Welcome to UPLUS, please use '.$code.' to log into your account.';
+	$data = array(
+		"sender"		=>$sender,
+		"recipients"	=>$recipients,
+		"message"		=>$message,
+	);
+	include 'sms.php';
+	if($httpcode == 200)
+	{
+		echo "yes";
+	}
+	else
+	{
+		echo 'System error';
+	}
 }
 ?>
