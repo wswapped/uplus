c2b8934f9c522c32ba57c1d814200e8d845fa1f7
Clement <muhirwaclement@gmail.com>
2017-10-25 03:11:42 +0200

    Update test.php

--- api/test.php
+++ api/test.php
@@ -1,15 +1,14 @@
 <?php
-icnlude 'db.php';
+include 'db.php';
 $pullNumber = '0784848236';
 $sql = $db->query("SELECT * FROM users WHERE phone = '$pullNumber' LIMIT 1");
 $checkAvailb = mysqli_num_rows($sql);
 if($checkAvailb > 0)
 {
-	//$row = mysqli_fetch_array($sql);
-	//echo $pullName	= $row['name'];
-	//echo $pullId	= $row['id'];
-	//echo $token		= $row['token'];
-	echo "here";
+	$row = mysqli_fetch_array($sql);
+	echo $pullName	= $row['name'];
+	echo $pullId	= $row['id'];
+	echo $token		= $row['token'];
 }else{
 	echo "out";
 }
