01550978828f5f6162fa7f898b1ef539be46d435
Clement <muhirwaclement@gmail.com>
2017-10-25 03:08:48 +0200

    Update test.php

--- api/test.php
+++ api/test.php
@@ -1,39 +1,14 @@
-<?php 
-transactions();
-function transactions()
+
+<?php
+icnlude "db.php";
+$pullNumber = '0784848236'
+$sql = $db->query("SELECT * FROM users WHERE phone = '$pullNumber' LIMIT 1");
+$checkAvailb = mysqli_num_rows($sql);
+if($checkAvailb > 0)
 {
-	require('db.php');
-	$userId	= 1;
-	$sql 	= $outCon->query("SELECT id, amount, status FROM `directtransfers` WHERE (`id` % 2) = 1 AND `userId` = '$userId' ORDER BY id DESC");
-	
-	$returnedinformation   = array();   
-	while ($row = mysqli_fetch_array($sql))
-	{
-		$pushId 	= $row['id'];
-		$pullId 	= $pushId + 1;
-		$amount 	= $row['amount'];
-		$status 	= $row['status'];
-		$sql2 		= $outCon->query("SELECT accountNumber, actorName, transaction_date FROM `directtransfers` WHERE `id` = '$pullId' LIMIT 1");
-		$row2 		= mysqli_fetch_array($sql2);
-		$pullName 	= $row2['actorName'];
-		$pullPhone 	= $row2['accountNumber'];
-		$transactionDate 	= strftime("%b, %d", strtotime($row2["transaction_date"]));
-
-		// GET THE USER TRANSATIONS
-		
-		$returnedinformation[] = array(
-				"amount" 	=> $amount,
-		        "pullName" 	=> $pullName,
-		        "phone" 	=> $pullPhone,
-		        "status"	=> $status,
-		        "transactionDate"	=> $transactionDate
-		    );
-		
-	}
-	
-	header('Content-Type: application/json');
-	$returnedinformation = json_encode($returnedinformation);
-	echo $returnedinformation;
-	}
-
-	?>
+	$row = mysqli_fetch_array($sql);
+	echo $pullName	= $row['name'];
+	echo $pullId		= $row['id'];
+	echo $token		= $row['token'];
+}
+?>
