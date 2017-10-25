244a6021f4ee9d0293409cb325875ea0cf1a5c98
Clement <muhirwaclement@gmail.com>
2017-10-25 03:33:11 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1173,7 +1173,8 @@ else
 			
 			$message = array("message" => "Test");
 			$message_status = send_notification($tokens, $message);
-
+			header('Content-Type: application/json');
+			echo $message_status;
 
 		}
 		else
