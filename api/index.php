9dafeb08a8e93ba6faa33044c958d8c946774ec8
Clement <muhirwaclement@gmail.com>
2017-10-25 03:31:09 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1129,10 +1129,52 @@ else
 			$pullName	= $row['name'];
 			$pullId	= $row['id'];
 
-			$_POST['selection'] = 'single';
-			$_POST['userId'] 	= $pullId;
-			notification();
-			//include 'db.php';
+			
+			function send_notification ($tokens, $message)
+			{
+				$url = 'https://fcm.googleapis.com/fcm/send';
+				$fields = array(
+					 'registration_ids' => $tokens,
+					 'data' => $message
+					);
+
+				$headers = array(
+					'Authorization:key = AIzaSyCVsbSeN2qkfDfYq-IwKrnt05M1uDuJxjg',
+					'Content-Type: application/json'
+					);
+
+			   $ch = curl_init();
+		       curl_setopt($ch, CURLOPT_URL, $url);
+		       curl_setopt($ch, CURLOPT_POST, true);
+		       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		       curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);  
+		       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		       curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+		       $result = curl_exec($ch);           
+		       if ($result === FALSE) {
+		           die('Curl failed: ' . curl_error($ch));
+		       }
+		       curl_close($ch);
+		       return $result;
+			}	
+
+			$userId 		= $pullId;
+			$sql = $db->query("SELECT token FROM users WHERE id = '$userId'");
+			
+			$tokens = array();
+
+			if(mysqli_num_rows($sql) > 0 ){
+				while ($row = mysqli_fetch_assoc($sql)) {
+					$tokens[] = $row["token"];
+				}
+			}
+
+			
+			$message = array("message" => "Test");
+			$message_status = send_notification($tokens, $message);
+
+
 		}
 		else
 		{
