3c40cd19cf3dfd9cfcf52bb5a59f71b158d6dbac
Clement <cmuhirwa@users.noreply.github.com>
2017-10-28 02:23:05 +0200

    send the db

--- monitor/db.php
+++ monitor/db.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL); 
 ini_set('display_errors', 1);
-		$con = mysqli_connect("localhost:8889", "clement", "clement123" , "rtgs");
-		$db = mysqli_connect("localhost:8889", "clement", "clement123" , "uplus");
+		$con = mysqli_connect("localhost", "clement", "clement123" , "rtgs");
+		$db = mysqli_connect("localhost", "clement", "clement123" , "uplus");
 ?>
