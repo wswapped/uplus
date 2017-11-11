748e08b7300ce7e6b019c5384e549b8325c7395d
Clement <muhirwaclement@gmail.com>
2017-11-11 14:20:31 +0200

    Update index.php

--- api/ussd/index.php
+++ api/ussd/index.php
@@ -6,50 +6,49 @@ $phoneNumber = $_POST["phoneNumber"];
 $text        = $_POST["text"];
 
 
-if ( $text == "" || $text == "#" || $text == "1*#" ) {
-
+if ( $text == "" || $text == "#" || $text == "1*#" ) 
+{
+  require('../db.php');
   //CLEAN PHONE
     $phoneNumber  = preg_replace( '/[^0-9]/', '', $phoneNumber );
     $phoneNumber  = substr($phoneNumber, -10); 
     $sql  = $db->query("SELECT * FROM users WHERE phone = '$phoneNumber' LIMIT 1");
     if($db)
     {
-      $countPin     = mysqli_num_rows($sql);
-      $code         = rand(1000, 9999);
-      $signInfo     = array();
-      if($countPin > 0)
-      {
-          $userData = mysqli_fetch_array($sql);
-          $userName = $userData['name'];
-          $userId = $userData['id'];
-      }
-      else
-      {
+        $countPin     = mysqli_num_rows($sql);
+        $code         = rand(1000, 9999);
+        $signInfo     = array();
+        if($countPin > 0)
+        {
+            $userData = mysqli_fetch_array($sql);
+            $userName = $userData['name'];
+            $userId = $userData['id'];
+        }
+        else
+        {
+            $sqlsavePin = $db->query("INSERT INTO `users`(
+            phone, active, createdDate, password, visits, updatedBy, updatedDate) 
+            VALUES('$phoneNumber', '0', now(), '$code', '0', '1', now())")or die (mysqli_error());
+            $sqlcheckPin = $db->query("SELECT * FROM users ORDER BY id DESC LIMIT 1");
+            $userData = mysqli_fetch_array($sqlcheckPin);
+            $userName = $userData['name'];
+            $userId = $userData['id'];
+        }
         
-        $sqlsavePin = $db->query("INSERT INTO `users`(
-        phone, active, createdDate, password, visits, updatedBy, updatedDate) 
-        VALUES('$phoneNumber', '0', now(), '$code', '0', '1', now())")or die (mysqli_error());
-
-        $sqlcheckPin = $db->query("SELECT * FROM users ORDER BY id DESC LIMIT 1");
-          $userData = mysqli_fetch_array($sqlcheckPin);
-          $userName = $userData['name'];
-          $userId = $userData['id'];
-      }
-      
-      $response  = "CON Murakazaneza Kukimina Cya Uplus ".$userName." \n";
-      
-      // This is the first request. Note how we start the response with CON
+        $response  = "CON Murakazaneza Kukimina Cya Uplus ".$userName." \n";
+        // This is the first request. Note how we start the response with CON
     }
     // Business logic for first level response
     $response .= "1. Ibimina Ndimo \n";
     $response .= "2. Ubusobanuro \n";
     $response .= "#. Subira Inyuma";
-   
 }
-else if ( $text == "1" ) {
 
-  //USER DATA
-  require('../db.php');
+else if ( $text == "1" ) 
+{
+
+    //USER DATA
+    require('../db.php');
     //CLEAN PHONE
     $phoneNumber  = preg_replace( '/[^0-9]/', '', $phoneNumber );
     $phoneNumber  = substr($phoneNumber, -10); 
@@ -96,30 +95,34 @@ else if ( $text == "1" ) {
     $response .= "#. Subira Inyuma";
 }
 
-else if($text == "2") {
+else if($text == "2") 
+{
   // Business logic for first level response
   // This is a terminal request. Note how we start the response with END
   $response = "CON Uplus ni apurikasiyo imanajinga Ibimina n'Amakoperative";
  }
 
-else if($text == "1*1") {
-  // This is a second level response where the user selected 1 in the first instance
-  $response   = "CON Ikimina Ubumwe \n";
-  $response  .= "1. Tanga Umusanzu \n";
-  $response  .= "2. Saba Inguzanyo \n";
-  $response  .= "3. Ishyura Inguzanyo \n";
-  $response  .= "4. Konti Yawe ";
-  // This is a terminal request. Note how we start the response with END
- }
-    
- else if ( $text == "1*2" ) {
-  
-     // This is a second level response where the user selected 1 in the first instance
-     $balance  = "RWF 10,000";
-     // This is a terminal request. Note how we start the response with END
-     $response = "CON END Your balance is $balance";
+else if($text == "1*1") 
+{
+    // This is a second level response where the user selected 1 in the first instance
+    $response   = "CON Ikimina Ubumwe \n";
+    $response  .= "1. Tanga Umusanzu \n";
+    $response  .= "2. Saba Inguzanyo \n";
+    $response  .= "3. Ishyura Inguzanyo \n";
+    $response  .= "4. Konti Yawe ";
+    // This is a terminal request. Note how we start the response with END
 }
-else if ( $text == "1*1*1" ) {
+    
+else if ( $text == "1*2" ) 
+{
+    // This is a second level response where the user selected 1 in the first instance
+    $balance  = "RWF 10,000";
+    // This is a terminal request. Note how we start the response with END
+    $response = "CON END Your balance is $balance";
+}
+
+else if ( $text == "1*1*1" ) 
+{
   // This is a second level response where the user selected 1 in the first instance
   $accountNumber  = "CON 1. Tanga Umusanzu \n";
   $accountNumber  = "2. Saba Inguzanyo \n";
@@ -127,15 +130,17 @@ else if ( $text == "1*1*1" ) {
   $accountNumber  = "4. Konti Yawe ";
   // This is a terminal request. Note how we start the response with END
   $response = "END Your account number is $accountNumber";
- }
-else if ( $text == "1*1*4" ) {
+}
+
+else if ( $text == "1*1*4" ) 
+{
   // This is a second level response where the user selected 1 in the first instance
   $accountNumber  = "CON Nashoye: \n";
   $accountNumber  .= "Inguzanyo: \n";
   $accountNumber  .= "1. Ubone Ubutumwa burambuye bwa konti yawe, nb: urishyura 10Rwf ya SMS ";
   // This is a terminal request. Note how we start the response with END
   $response = "END Your account number is $accountNumber";
- }
+}
 // Print the response onto the page so that our gateway can read it
 header('Content-type: text/plain');
 echo $response;
