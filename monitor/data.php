7dd9a576d253937a7eb4540b023b368e87c48e09
Clement <muhirwaclement@gmail.com>
2017-11-17 22:28:58 +0200

    Update data.php

--- monitor/data.php
+++ monitor/data.php
@@ -1,26 +1,32 @@
 <?php
-
-#Basic Line
-require 'db.php';
-
-$result = $con->query(" SELECT COUNT( * ) AS JUMLAH,DATE(transaction_date) BULAN
-	FROM directtransfers 
-    WHERE (`id` % 2) = 1
-    AND transaction_date BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()
-    group by DATE(transaction_date)");
-
-
-$bln = array();
-$bln['name'] = 'Dates';
-$rows['name'] = 'Transfers';
-while ($r = mysqli_fetch_array($result)) {
-    $bln['data'][] = strftime("%d %b", strtotime($r['BULAN']));
-    $rows['data'][] = $r['JUMLAH'];
+if (isset($_GET['datapage'])) 
+{
+    $_GET['datapage']();
 }
-$rslt = array();
-array_push($rslt, $bln);
-array_push($rslt, $rows);
-print json_encode($rslt, JSON_NUMERIC_CHECK);
 
+function transactions()
+{
+	#Basic Line
+	require 'db.php';
+
+	$result = $con->query(" SELECT COUNT( * ) AS JUMLAH,DATE(transaction_date) BULAN
+		FROM directtransfers 
+	    WHERE (`id` % 2) = 1
+	    AND transaction_date BETWEEN CURDATE() - INTERVAL 30 DAY AND CURDATE()
+	    group by DATE(transaction_date)");
+
+
+	$bln = array();
+	$bln['name'] = 'Dates';
+	$rows['name'] = 'Transfers';
+	while ($r = mysqli_fetch_array($result)) {
+	    $bln['data'][] = strftime("%d %b", strtotime($r['BULAN']));
+	    $rows['data'][] = $r['JUMLAH'];
+	}
+	$rslt = array();
+	array_push($rslt, $bln);
+	array_push($rslt, $rows);
+	print json_encode($rslt, JSON_NUMERIC_CHECK);
+}
 ?>
 
