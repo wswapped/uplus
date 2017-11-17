fcd68380e51c224293d3339fc6259f5eec5b0eda
Clement <muhirwaclement@gmail.com>
2017-11-17 22:47:55 +0200

    Update chart.php

--- monitor/chart.php
+++ monitor/chart.php
@@ -63,4 +63,187 @@ function transactions()
         <div id="container" style="min-width: 400px; height: 400px; margin: 0 auto"></div>
     </body>
 </html>
+<?php }
+function users()
+{
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <script type="text/javascript" src="js/jquery.min1.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                var options = {
+                    chart: {
+                        renderTo: 'container',
+                        type: 'area'
+                    },
+                    title: {
+                        text: '',
+                        x: -20 //center
+                    },
+                    xAxis: {
+                        categories: [],
+                        title: {
+                            text: 'Days'
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Bids'
+                        },
+                        plotLines: [{
+                                value: 0,
+                                width: 1,
+                                color: '#808080'
+                            }]
+                    },
+                    tooltip: {
+                        valueSuffix: 'Transaction date'
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+                    series: []
+                };
+                $.getJSON("data.php?datapage=users", function(json) {
+                    options.xAxis.categories = json[0]['data']; //xAxis: {categories: []}
+                    options.series[0] = json[1];
+                    chart = new Highcharts.Chart(options);
+                });
+            });
+        </script>
+        <script src="js/highcharts1.js"></script>
+        <script src="js/exporting.js"></script>
+    </head>
+    <body>
+        <div id="container" style="min-width: 400px; height: 400px; margin: 0 auto"></div>
+    </body>
+</html>
+<?php }
+function groups()
+{
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <script type="text/javascript" src="js/jquery.min1.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                var options = {
+                    chart: {
+                        renderTo: 'container',
+                        type: 'area'
+                    },
+                    title: {
+                        text: '',
+                        x: -20 //center
+                    },
+                    xAxis: {
+                        categories: [],
+                        title: {
+                            text: 'Days'
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Bids'
+                        },
+                        plotLines: [{
+                                value: 0,
+                                width: 1,
+                                color: '#808080'
+                            }]
+                    },
+                    tooltip: {
+                        valueSuffix: 'Transaction date'
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+                    series: []
+                };
+                $.getJSON("data.php?datapage=groups", function(json) {
+                    options.xAxis.categories = json[0]['data']; //xAxis: {categories: []}
+                    options.series[0] = json[1];
+                    chart = new Highcharts.Chart(options);
+                });
+            });
+        </script>
+        <script src="js/highcharts1.js"></script>
+        <script src="js/exporting.js"></script>
+    </head>
+    <body>
+        <div id="container" style="min-width: 400px; height: 400px; margin: 0 auto"></div>
+    </body>
+</html>
+<?php }
+function money()
+{
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <script type="text/javascript" src="js/jquery.min1.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                var options = {
+                    chart: {
+                        renderTo: 'container',
+                        type: 'area'
+                    },
+                    title: {
+                        text: '',
+                        x: -20 //center
+                    },
+                    xAxis: {
+                        categories: [],
+                        title: {
+                            text: 'Days'
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Bids'
+                        },
+                        plotLines: [{
+                                value: 0,
+                                width: 1,
+                                color: '#808080'
+                            }]
+                    },
+                    tooltip: {
+                        valueSuffix: 'Transaction date'
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+                    series: []
+                };
+                $.getJSON("data.php?datapage=money", function(json) {
+                    options.xAxis.categories = json[0]['data']; //xAxis: {categories: []}
+                    options.series[0] = json[1];
+                    chart = new Highcharts.Chart(options);
+                });
+            });
+        </script>
+        <script src="js/highcharts1.js"></script>
+        <script src="js/exporting.js"></script>
+    </head>
+    <body>
+        <div id="container" style="min-width: 400px; height: 400px; margin: 0 auto"></div>
+    </body>
+</html>
 <?php } ?>
