327e5d6033e630c2dfcf46a604a3135e39caa250
Clement <muhirwaclement@gmail.com>
2017-11-18 18:54:13 +0200

    Update mysqldump.sh

--- docs/backups/mysqldump.sh
+++ docs/backups/mysqldump.sh
@@ -1,1 +1,1 @@
-mysqldump -u clement -p clement123 uplus > /var/www/html/docs/backups/mysqldb_`date +%F`.sql
+mysqldump -u clement -p<mysql_root_password> uplus > /var/www/html/docs/backups/mysqldb_`date +%F`.sql
