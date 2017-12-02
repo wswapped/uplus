4f8af3e0bf3fbd3d6c93a8ca6e75f34f40f0d331
Clement <muhirwaclement@gmail.com>
2017-12-02 11:18:08 +0200

    Update index.php

--- api/ussd/index.php
+++ api/ussd/index.php
@@ -1,148 +1,136 @@
 <?php
-// Reads the variables sent via POST from our gateway
-$sessionId   = $_POST["sessionId"];
-$serviceCode = $_POST["serviceCode"];
-$phoneNumber = $_POST["phoneNumber"];
-$text        = $_POST["text"];
+include_once "dbcons.php";
+header("Content-Type: text/plain");
+session_start(); //For web testing only
+//Initialising
+$response = "";
+$tdata = array();
+$session_id = rand(10000, 99999);
+$session_id = session_id();
+
+$req = array_merge($_POST, $_GET); //Keeping get and post for testing and productin handling concurently
+
+$sessionId   = $req["sessionId"]??$session_id;
+$serviceCode = $req["serviceCode"]??"*801#";
+$phoneNumber = $req["phoneNumber"];
+$text        = $req["text"];
+
+//IN USSD phone number is always sent
+//CLEAN and sanitize PHONE
+$phoneNumber  = preg_replace( '/[^0-9]/', '', $phoneNumber );
+$phoneNumber  = substr($phoneNumber, -10);
+
+	//Checking if user exists
+	$query  = mysqli_query($conn, "SELECT * FROM users WHERE phone = '$phoneNumber' LIMIT 1");
+
+	if(mysqli_num_rows($query))
+	{
+		//Here user already exists		
+		$signInfo     = array();
+		$userData = mysqli_fetch_array($query);
+		$userName = $userData['name'];
+		$userId = $userData['id'];
+		echo "$userName";
+	}
+	else{
+		//Here the user is new, should I ask the name?
+		$code         = rand(1000, 9999);
+	    $sqlsavePin = $db->query("INSERT INTO `users`(
+	    phone, active, createdDate, password, visits, updatedBy, updatedDate) 
+	    VALUES('$phoneNumber', '0', now(), '$code', '0', '1', now())")or die (mysqli_error());
+
+	    $sqlcheckPin = $db->query("SELECT * FROM users ORDER BY id DESC LIMIT 1");
+	      $userData = mysqli_fetch_array($sqlcheckPin);
+	      $userName = $userData['name'];
+	      $userId = $userData['id'];
+	}
+
+	if(empty($text) || $text == "#" || $text == "1*#"){
+		//Beginning of application
+		//Checking groups a user is in
+		$query = mysqli_query($conn, "SELECT groupId, groupName FROM `members` WHERE memberPhone = \"$phoneNumber\"") or die("Error getting groups you belong in, ".mysqli_error($conn));
+		$groups = array();
+
+		//Looping through all groups and putting them in $groups arry
+		while ($temp = mysqli_fetch_assoc($query)) {
+			$groups[] = $temp['groupName'];
+			$groupsIds[] = $temp['groupId'];
+		}
+		if(empty($groups)){
+			$response =  "CON Nta gurupe urimo.\nKugirango ujye muri gurupe shyiramo umubare uyiranga\n";
+			//TODO:Handle the group input
+		}else{
+			//Showing groups
+			$response.="CON Murakaza Neza kuri Uplus!\nHitamo gurupe\n";
+			for($n=0; $n<count($groups); $n++){
+				//Storing details we're showing for proof of selected group
+				$tdata[($n+1)] = $groupsIds[$n];
+
+				$response.=($n+1).". $groups[$n]\n";
+			}
+
+			//Logging the tempdata
+			keeptempdata($session_id, $tdata, 'groups');
+		}
+		echo "$response";
+	}else{
+		$requests = explode("*", $text);
+		
+		//Number of requests
+		$nrequests = count($requests);
+		if($nrequests == 1){
+			//Here going to handle first request, going to check if sent text is among the groups shown
+			$query = mysqli_query($conn, "SELECT * FROM ussdTempData WHERE session_id = \"$session_id\" and type = 'groups' ORDER BY time DESC LIMIT 1") or die(mysqli_error($conn));
+			$data = mysqli_fetch_assoc($query);			
+				
+			$data = json_decode($data['data'], true);
+
+			//There is problem accessing this array with strings which PHP keeps changing to number, here's  work around
+			$temp = $data;
+			$data = array();
+			foreach ($temp as $key => $value) {
+				$data[$key] = $value;
+			}
+
+			if(!empty($data[$text])){
+				//Here the user chose a group presented
+				$groupid  = $data[$text];
+
+				//Getting group members and name
+				$query = mysqli_query($conn, "SELECT * FROM members WHERE groupId = \"$groupid\"") or die("Error: ".mysqli_error($conn));
+				$membersOrder  = $groupInfo = array();
+				$n=0;
+				while ($temp = mysqli_fetch_assoc($query)) {
+					if($n==0){
+						$response = "CON Ikaze muri $temp[groupName]\n"; //Printing the group on first loop
+						$groupName = $temp['groupName'];
+					}
+					$n++;
+					$groupInfo[] = $temp;
+
+					$response.="$n $temp[memberName]\n";
+
+					//Storing order of group memebrs
+					$membersOrder[$n] = $temp['memberId'];
+				}
+				keeptempdata($session_id, $data, "$groupName members");
+
+				//Logging the members
+				echo "$response";
 
 
-if ( $text == "" || $text == "#" || $text == "1*#" ) 
-{
-  require('../db.php');
-  //CLEAN PHONE
-    $phoneNumber  = preg_replace( '/[^0-9]/', '', $phoneNumber );
-    $phoneNumber  = substr($phoneNumber, -10); 
-    $sql  = $db->query("SELECT * FROM users WHERE phone = '$phoneNumber' LIMIT 1");
-    if($db)
-    {
-        $countPin     = mysqli_num_rows($sql);
-        $code         = rand(1000, 9999);
-        $signInfo     = array();
-        if($countPin > 0)
-        {
-            $userData = mysqli_fetch_array($sql);
-            $userName = $userData['name'];
-            $userId = $userData['id'];
-        }
-        else
-        {
-            $sqlsavePin = $db->query("INSERT INTO `users`(
-            phone, active, createdDate, password, visits, updatedBy, updatedDate) 
-            VALUES('$phoneNumber', '0', now(), '$code', '0', '1', now())")or die (mysqli_error());
-            $sqlcheckPin = $db->query("SELECT * FROM users ORDER BY id DESC LIMIT 1");
-            $userData = mysqli_fetch_array($sqlcheckPin);
-            $userName = $userData['name'];
-            $userId = $userData['id'];
-        }
-        
-        $response  = "CON Hello!, Murakazaneza Kukimina Cya Uplus ".$userName." \n";
-        // This is the first request. Note how we start the response with CON
-    }
-    // Business logic for first level response
-    $response .= "1. Ibimina Ndimo \n";
-    $response .= "2. Ubusobanuro \n";
-    $response .= "#. Subira Inyuma";
-}
+			}	
+		}
+	}
 
-else if ( $text == "1" ) 
-{
-
-    //USER DATA
-    require('../db.php');
-    //CLEAN PHONE
-    $phoneNumber  = preg_replace( '/[^0-9]/', '', $phoneNumber );
-    $phoneNumber  = substr($phoneNumber, -10); 
-    $sql  = $db->query("SELECT * FROM users WHERE phone = '$phoneNumber' LIMIT 1");
-    if($db)
-    {
-      $countPin     = mysqli_num_rows($sql);
-      $code         = rand(1000, 9999);
-      $signInfo     = array();
-      if($countPin > 0)
-      {
-          $userData = mysqli_fetch_array($sql);
-          $userName = $userData['name'];
-          $userId = $userData['id'];
-      }
-      else
-      {
-        
-        $sqlsavePin = $db->query("INSERT INTO `users`(
-        phone, active, createdDate, password, visits, updatedBy, updatedDate) 
-        VALUES('$phoneNumber', '0', now(), '$code', '0', '1', now())")or die (mysqli_error());
-
-        $sqlcheckPin = $db->query("SELECT * FROM users ORDER BY id DESC LIMIT 1");
-          $userData = mysqli_fetch_array($sqlcheckPin);
-          $userName = $userData['name'];
-          $userId = $userData['id'];
-      }
-      
-      $response  = "CON Murakazaneza Kukimina Cya Uplus ".$userName." \n";
-      $n = 0;
-      $sqlgroups  = $db->query("SELECT groupName FROM members WHERE memberId = '$userId' LIMIT 1");
-      if(mysqli_num_rows($sqlgroups)>0)
-      WHILE($group  = mysqli_fetch_array($sqlgroups))
-      {
-        $n++;
-        $response .= $n.". ".$group['groupName']." \n";
-      }
-      else{
-        $response .= "Nta Kimina urimo \n";
-      }
-      // This is the first request. Note how we start the response with CON
-    }
-    $response .= "0. Jya mu Ikimina \n";
-    $response .= "#. Subira Inyuma";
-}
-
-else if($text == "2" || $text == "1*#*2") 
-{
-    // Business logic for first level response
-    // This is a terminal request. Note how we start the response with END
-    $response = "CON Uplus ni apurikasiyo imanajinga Ibimina nAmakoperative";
- }
-
-else if($text == "1*1") 
-{
-    // This is a second level response where the user selected 1 in the first instance
-    $response   = "CON Ikimina Ubumwe \n";
-    $response  .= "1. Tanga Umusanzu \n";
-    $response  .= "2. Saba Inguzanyo \n";
-    $response  .= "3. Ishyura Inguzanyo \n";
-    $response  .= "4. Konti Yawe ";
-    // This is a terminal request. Note how we start the response with END
-}
-    
-else if ( $text == "1*2" ) 
-{
-    // This is a second level response where the user selected 1 in the first instance
-    $balance  = "RWF 10,000";
-    // This is a terminal request. Note how we start the response with END
-    $response = "CON END Your balance is $balance";
-}
-
-else if ( $text == "1*1*1" ) 
-{
-  // This is a second level response where the user selected 1 in the first instance
-  $accountNumber  = "CON 1. Tanga Umusanzu \n";
-  $accountNumber  = "2. Saba Inguzanyo \n";
-  $accountNumber  = "3. Ishyura Inguzanyo \n";
-  $accountNumber  = "4. Konti Yawe ";
-  // This is a terminal request. Note how we start the response with END
-  $response = "END Your account number is $accountNumber";
-}
-
-else if ( $text == "1*1*4" ) 
-{
-  // This is a second level response where the user selected 1 in the first instance
-  $accountNumber  = "CON Nashoye: \n";
-  $accountNumber  .= "Inguzanyo: \n";
-  $accountNumber  .= "1. Ubone Ubutumwa burambuye bwa konti yawe, nb: urishyura 10Rwf ya SMS ";
-  // This is a terminal request. Note how we start the response with END
-  $response = "END Your account number is $accountNumber";
-}
-// Print the response onto the page so that our gateway can read it
-header('Content-type: text/plain');
-echo $response;
-// DONE!!!
+	function keeptempdata($session_id, $data, $type){
+		global $conn;
+		$data = json_encode($data);
+		$data = mysqli_real_escape_string($conn, $data);
+		$sql = "INSERT INTO ussdTempData(session_id, data, type) VALUES(\"$session_id\", \"$data\", \"$type\")";
+		$query = mysqli_query($conn, $sql) or die("Can't log data: ".mysqli_error($conn));
+		if($query)
+			return true;
+		else return false;
+	}
 ?>
