80fbc8c6c1221b7f7431fecd9c7bf237fce9b9e3
Clement <muhirwaclement@gmail.com>
2017-12-03 17:06:26 +0200

    Update index.php

--- api/ussd/index.php
+++ api/ussd/index.php
@@ -4,7 +4,7 @@ header("Content-Type: text/plain");
 session_start(); //For web testing only
 
 
-//FOr browser testing
+//For browser-based testing
 if(isset($_GET['ses'])){
 	session_destroy();
 	session_start();
@@ -54,37 +54,104 @@ $phoneNumber  = substr($phoneNumber, -10);
 	}else{
 		//Handling further requests
 		$requests = explode("*", $text);
+		$nrequests = count($requests); //Number of requests
 
 		//Level1 requests
 		if(is_numeric($requests[0])){
 			//Handling first menu
 			$fmenu = $requests[0];
 			if($fmenu == 1){
-				//Checking for groups a user is in
-				$query = mysqli_query($conn, "SELECT groupId, groupName FROM `members` WHERE memberPhone = \"$phoneNumber\"") or die("Error getting groups you belong in, ".mysqli_error($conn));
-				$groups = array();
 
-				//Looping through all groups and putting them in $groups arry
-				while ($temp = mysqli_fetch_assoc($query)) {
-					$groups[] = $temp['groupName'];
-					$groupsIds[] = $temp['groupId'];
-				}
+				if($nrequests == 1){
+					//Checking for groups a user is in
+					$query = mysqli_query($conn, "SELECT groupId, groupName FROM `members` WHERE memberPhone = \"$phoneNumber\"") or die("Error getting groups you belong in, ".mysqli_error($conn));
+					// $groups = array();
 
-				if(empty($groups)){
-					//User does not belong in any group
-					$response =  "CON ".$userName." Nta gurupe urimo.\nKugirango ujye muri gurupe shyiramo umubare uyiranga\n";
-					//TODO:Handle the group input in third input
-				}else{
-					//Showing groups
-					$response.="CON ".$userName.", Hitamo gurupe\n";
-					for($n=0; $n<count($groups); $n++){
-						//Storing details we're showing for proof of selected group
-						$tdata[($n+1)] = $groupsIds[$n];
-						$response.=($n+1).". $groups[$n]\n";
+					// //Looping through all groups and putting them in $groups arry
+					// while ($temp = mysqli_fetch_assoc($query)) {
+					// 	$groups[] = $temp['groupName'];
+					// 	$groupsIds[] = $temp['groupId'];
+					// }
+					$groups = usergroups($phoneNumber);
+
+					if(empty($groups)){
+						//User does not belong in any group
+						$response =  "CON ".$userName." Nta gurupe urimo.\nKugirango ujye muri gurupe shyiramo umubare uyiranga\n";
+					}else{
+						//Showing groups
+						$response.="CON ".$userName.", Hitamo gurupe\n";
+
+						$n=0;
+						foreach ($groups as $groupid => $groupname) {
+							$n++;
+							$tdata[$n] = $groupid;
+							$response .= "$n $groupname\n";
+						}
+						//Logging the tempdata
+						keeptempdata($sessionId, $tdata, 'groups');
 					}
-					//Logging the tempdata
-					keeptempdata($sessionId, $tdata, 'groups');
+				}else{
+					//Further requests were issued
+					$smenu = $requests[1];
+
+					if($nrequests == 2){
+						//Here going to handle first request, going to check if sent text is among the groups shown
+						$sql = "SELECT * FROM ussdtempdata WHERE session_id = '$sessionId' and type = 'groups' ORDER BY time DESC LIMIT 1";
+						$query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+						$data = mysqli_fetch_assoc($query);
+						
+						$data = json_decode($data['data'], true);
+						if(is_array($data)  )
+						{
+							if (empty($data)) {
+							    // decoded is empty.
+								$response =  "CON empty decodded";
+							}else
+							{
+								if(!empty($data[$smenu]))
+								{
+									//Here the user chose a group presented
+									$groupid  = $data[$smenu];
+									//Getting group members and name
+									$query = mysqli_query($conn, "SELECT groupName, memberId, COALESCE(`memberName`, `memberPhone`) `memberName` FROM members WHERE groupId = \"$groupid\"") or die("Error: ".mysqli_error($conn));
+									$membersOrder  = $groupInfo = array();
+									$n=0;
+									while ($temp = mysqli_fetch_assoc($query)) 
+									{
+										if($n==0){
+											$response = "CON Ikaze muri $temp[groupName]\n"; //Printing the group on first loop
+											$groupName = $temp['groupName'];
+										}
+										$n++;
+										$groupInfo[] = $temp;
+										$response.="$n $temp[memberName]\n";
+										//Storing order of group memebrs
+										$membersOrder[$n] = $temp['memberId'];
+									}
+									keeptempdata($sessionId, $data, "$groupName members");
+									//Logging the members
+									
+								}else{
+									//Maybe the user put the group ID
+									//checking if text is a group ID
+									$query = $db->query("SELECT groupName FROM groups WHERE id = \"$text\" LIMIT 1") or die("Can not get group you want");
+									if(mysqli_num_rows($query)){
+										//Here the group*
+									}
+								}
+							}
+						}
+						else
+						{
+							$response =  "CON Received content contained invalid JSON!".var_dump($temp);
+						}
+					}else{
+						//Group was chose or group code was input
+						echo "string";
+					}
+					
 				}
+				
 			}
 
 
@@ -115,8 +182,7 @@ $phoneNumber  = substr($phoneNumber, -10);
 	// 		$query = mysqli_query($conn, $sql) or die(mysqli_error($conn));
 	// 		$data = mysqli_fetch_assoc($query);
 			
-	// 		$data = json_decode($data['data'], true);
-			
+	// 		$data = json_decode($data['data'], true); 			
 
 	// 		//There is problem accessing this array with strings which PHP keeps changing to number, here's  work around
 	// 		if(is_array($data)  )
@@ -165,7 +231,7 @@ $phoneNumber  = substr($phoneNumber, -10);
 	// 			$response =  "CON Received content contained invalid JSON!".var_dump($temp);
 	// 		}
 	// 	}
-	// }
+	// // }
 	function keeptempdata($session_id, $data, $type){
 		global $conn;
 		$data = json_encode($data);
@@ -175,6 +241,22 @@ $phoneNumber  = substr($phoneNumber, -10);
 		if($query)
 			return true;
 		else return false;
+	}
+	function usergroups($phone){
+		//FUnction to check the groups a user with $phone belongs in
+		global $conn;
+		$sql = "SELECT * FROM members WHERE memberPhone = \"$phone\"";
+
+		$query = mysqli_query($conn, $sql) or die("END Error Checking groups u belong in\n".mysqli_error($conn));
+
+
+		//Looping through all groups and putting them in $groups array
+		$groups = array();
+		while ($temp = mysqli_fetch_assoc($query)) {
+			$groups[$temp['groupId']] = $temp['groupName'];
+		}
+
+		return $groups;
 	}
 	echo "$response";
 ?>
