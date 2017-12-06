e3e398bee39b948bef4725f32c714c0b2c2e7a4a
Clement <muhirwaclement@gmail.com>
2017-12-06 19:21:55 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1286,65 +1286,65 @@
 				$pushTransactionId = $pullTransactionId - 1;
 
 				
-				//CHECK BALANCE
-				$url = 'https://www.intouchpay.co.rw/api/getbalance/';
+				// //CHECK BALANCE
+				// $url = 'https://www.intouchpay.co.rw/api/getbalance/';
 
-				$username="muhirwa.clement";
-				$var_time = time();
-				$generate =  $username.'250150000003'.'8;b%-#K2$w\J3q{^dwr'.$var_time;
-				$generate_hash =  hash('sha256', $generate);
-				$txt_id = md5(time());
-				$data = array();
-				$data["username"] 				= $username;
-				$data["password"] 				= $generate_hash;
-				$data["timestamp"] 				= $var_time;
-				$options = array(
-					'http' => array(
-						'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-						'method'  => 'POST',
-						'content' => http_build_query($data)
-					)
-				);
-				$context  = stream_context_create($options);
-				$result = file_get_contents($url, false, $context);
-				//echo $result;
-				if ($result === FALSE) 
-				{ 
-					$returnedinformation	= array();
-					$returnedinformation[] = array(
-				       		"status" => "NETWORK ERROR"
-				    	);
-					header('Content-Type: application/json');
-					$returnedinformation = json_encode($returnedinformation);
-					echo $returnedinformation;
-				}
-				else
-				{
-					$result = json_decode($result);
-					//Prepare data for db
-					$success 				= $result->{'success'};
+				// $username="muhirwa.clement";
+				// $var_time = time();
+				// $generate =  $username.'250150000003'.'8;b%-#K2$w\J3q{^dwr'.$var_time;
+				// $generate_hash =  hash('sha256', $generate);
+				// $txt_id = md5(time());
+				// $data = array();
+				// $data["username"] 				= $username;
+				// $data["password"] 				= $generate_hash;
+				// $data["timestamp"] 				= $var_time;
+				// $options = array(
+				// 	'http' => array(
+				// 		'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+				// 		'method'  => 'POST',
+				// 		'content' => http_build_query($data)
+				// 	)
+				// );
+				// $context  = stream_context_create($options);
+				// $result = file_get_contents($url, false, $context);
+				// //echo $result;
+				// if ($result === FALSE) 
+				// { 
+				// 	$returnedinformation	= array();
+				// 	$returnedinformation[] = array(
+				//        		"status" => "NETWORK ERROR"
+				//     	);
+				// 	header('Content-Type: application/json');
+				// 	$returnedinformation = json_encode($returnedinformation);
+				// 	echo $returnedinformation;
+				// }
+				// else
+				// {
+				// 	$result = json_decode($result);
+				// 	//Prepare data for db
+				// 	$success 				= $result->{'success'};
 					
-					if($success == true)
-					{
-						$balance = $result->{'balance'};
-						$fee = ($amount*2)/100;
-						$charge = $fee + 120;
+				// 	if($success == true)
+				// 	{
+				// 		$balance = $result->{'balance'};
+				// 		$fee = ($amount*2)/100;
+				// 		$charge = $fee + 120;
 
 
-						if($balance < $charge)
-						{
-							$returnedinformation	= array();
-							$returnedinformation[] = array(
-						       		"status" 		=> "Failed",
-						       		"transactionid" => $pushTransactionId
-						    	);
-							header('Content-Type: application/json');
-							$returnedinformation = json_encode($returnedinformation);
-							echo $returnedinformation;
-							exit();
-						}
-					}
-				}
+				// 		if($balance < $charge)
+				// 		{
+				// 			$returnedinformation	= array();
+				// 			$returnedinformation[] = array(
+				// 		       		"status" 		=> "Failed",
+				// 		       		"transactionid" => $pushTransactionId
+				// 		    	);
+				// 			header('Content-Type: application/json');
+				// 			$returnedinformation = json_encode($returnedinformation);
+				// 			echo $returnedinformation;
+				// 			exit();
+				// 		}
+				// 	}
+				// }
 			
 
 				$url = 'https://www.intouchpay.co.rw/api/requestpayment/';
