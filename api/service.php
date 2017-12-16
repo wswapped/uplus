1da8e3a95bc02ef24451ca4232cb399ea9d9e19a
Clement <cmuhirwa@users.noreply.github.com>
2017-12-16 04:09:19 +0200

    service

---
+++ api/service.php
@@ -0,0 +1,39 @@
+<?php 
+include 'db.php';
+$sql = $outCon->query("SELECT id FROM grouptransactions WHERE status = 'Pending'") or die(mysql_error($outCon));
+echo"<ul>";
+$n=0;
+while($peniding = mysqli_fetch_array($sql))
+{
+	$n++;
+	$tobeup = $peniding['id'];
+	$url = 'https://uplus.rw/api/';
+					
+	$data = array();
+	
+	$data["action"] 				= "checkcontributionstatus";
+	$data["transactionId"] 			= $tobeup;
+
+    $options = array(
+		'http' => array(
+			'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+			'method'  => 'POST',
+			'content' => http_build_query($data)
+		)
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	if ($result == false) 
+	{ 
+		echo 'There was a system error, of:'.$tobeup;
+	}
+	else
+	{
+		$result = json_decode($result);	
+		$status = $result->{'status'};
+		$transactionId = $result->{'transactionId'};
+		echo "<li>".$n." ".$status." of: ".$transactionId."</li>";			
+	}
+}
+echo "</ul>";
+?>
