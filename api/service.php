03058ae78819ad2a006864a16e6e013c563296f8
Clement <cmuhirwa@users.noreply.github.com>
2017-12-16 04:30:46 +0200

    yes

--- api/service.php
+++ api/service.php
@@ -1,39 +1,81 @@
 <?php 
 include 'db.php';
-$sql = $outCon->query("SELECT id FROM grouptransactions WHERE status = 'Pending'") or die(mysql_error($outCon));
-echo"<ul>";
-$n=0;
-while($peniding = mysqli_fetch_array($sql))
+if (isset($_GET['contributions'])) 
 {
-	$n++;
-	$tobeup = $peniding['id'];
-	$url = 'https://uplus.rw/api/';
-					
-	$data = array();
-	
-	$data["action"] 				= "checkcontributionstatus";
-	$data["transactionId"] 			= $tobeup;
-
-    $options = array(
-		'http' => array(
-			'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-			'method'  => 'POST',
-			'content' => http_build_query($data)
-		)
-	);
-	$context  = stream_context_create($options);
-	$result = file_get_contents($url, false, $context);
-	if ($result == false) 
-	{ 
-		echo 'There was a system error, of:'.$tobeup;
-	}
-	else
+	$sql = $outCon->query("SELECT id FROM grouptransactions WHERE status = 'Pending'") or die(mysql_error($outCon));
+	echo"<ul>";
+	$n=0;
+	while($peniding = mysqli_fetch_array($sql))
 	{
-		$result = json_decode($result);	
-		$status = $result->{'status'};
-		$transactionId = $result->{'transactionId'};
-		echo "<li>".$n." ".$status." of: ".$transactionId."</li>";			
+		$n++;
+		$tobeup = $peniding['id'];
+		$url = 'https://uplus.rw/api/';
+						
+		$data = array();
+		
+		$data["action"] 				= "checkcontributionstatus";
+		$data["transactionId"] 			= $tobeup;
+
+	    $options = array(
+			'http' => array(
+				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+				'method'  => 'POST',
+				'content' => http_build_query($data)
+			)
+		);
+		$context  = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+		if ($result == false) 
+		{ 
+			echo 'There was a system error, of:'.$tobeup;
+		}
+		else
+		{
+			$result = json_decode($result);	
+			$status = $result->{'status'};
+			$transactionId = $result->{'transactionId'};
+			echo "<li>".$n." ".$status." of: ".$transactionId."</li>";			
+		}
 	}
+	echo "</ul>";
 }
-echo "</ul>";
+elseif (isset($_GET['directtransfers'])) 
+{
+	$sql = $outCon->query("SELECT id FROM directtransfers WHERE status = 'Pending'") or die(mysql_error($outCon));
+	echo"<ul>";
+	$n=0;
+	while($peniding = mysqli_fetch_array($sql))
+	{
+		$n++;
+		$tobeup = $peniding['id'];
+		$url = 'https://uplus.rw/api/';
+						
+		$data = array();
+		
+		$data["action"] 				= "checktransferstatus";
+		$data["transactionId"] 			= $tobeup;
+
+	    $options = array(
+			'http' => array(
+				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+				'method'  => 'POST',
+				'content' => http_build_query($data)
+			)
+		);
+		$context  = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+		if ($result == false) 
+		{ 
+			echo 'There was a system error, of:'.$tobeup;
+		}
+		else
+		{
+			$result = json_decode($result);	
+			$status = $result->{'status'};
+			$transactionId = $result->{'transactionId'};
+			echo "<li>".$n." ".$status." of: ".$transactionId."</li>";			
+		}
+	}
+	echo "</ul>";
+}
 ?>
