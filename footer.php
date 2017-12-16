21a418b2528a99bee656a09cc0ea6da0024f811a
Clement <cmuhirwa@users.noreply.github.com>
2017-12-16 03:45:26 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,39 @@
+
+<section class="section section_dark md-bg-blue-grey-700 footer" style="background:#007569 !important;padding: 48px 0 15px 0;">
+  <div class="uk-container uk-container-center">
+    <div class="uk-grid" data-uk-grid-margin>
+      <div class="uk-width-medium-4-10 uk-text-center-medium">
+        Get in touch
+        <br/><br/>
+        <div class="uk-align-medium-left uk-text-center-medium">
+          <a href="https://www.facebook.com/Uplus-950052218429354/" class="uk-margin-medium-right" data-uk-tooltip="{offset: 12}" title="Facebook">
+            <i class="uk-icon-facebook uk-icon-small md-color-white"></i>
+          </a>
+          <a href="https://twitter.com/uplusMP" class="uk-margin-medium-right" data-uk-tooltip="{offset: 12}" title="Twitter"><i class="uk-icon-twitter uk-icon-small md-color-white"></i></a>
+          <a href="#" class="uk-margin-medium-right" data-uk-tooltip="{offset: 12}" title="Youtube"><i class="uk-icon-youtube uk-icon-small md-color-white"></i></a>
+          <a href="#" data-uk-tooltip="{offset: 12}" title="Google Plus"><i class="uk-icon-google-plus uk-icon-small md-color-white"></i></a>
+        </div><br/><br/>
+        Copyright &copy; 2016 Uplus Mutual Partner ltd.
+      </div>
+      <div class="uk-width-medium-2-10 uk-text-center-medium">
+        <p>Uplus</p>
+        <a href="#">About</a><br/>
+        <a href="#">Fees</a><br/>
+        <a href="#">Security</a><br/>
+      </div>
+      <div class="uk-width-medium-2-10 uk-text-center-medium">
+        <p>Demo</p>
+        <a href="#">Social Contributions</a><br/>
+        <a href="#">Savings group USSD</a><br/>
+        <a href="#">Churches</a><br/>
+      </div>
+      <div class="uk-width-medium-2-10 uk-text-center-medium">
+        <p>Quick Links</p>
+        <a href="#">FAQ</a><br/>
+        <a href="#">PRESS</a><br/>
+      </div>
+    </div>
+  </div>
+</section>
+<div class="endfooter">
+</div>
