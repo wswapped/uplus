de4f96895f272f129c4c6dfffe4cba6b2509385c
Clement <cmuhirwa@users.noreply.github.com>
2017-12-24 04:04:26 +0200

    hey

--- monitor/tables.php
+++ monitor/tables.php
@@ -98,7 +98,7 @@ function groups()
     box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);
 	
 	"></div></th>
-				<th><a target="blank" href="./f/i'.$row['id'].'">'.$row['groupName'].'</a></th>
+				<th><a target="blank" href="../f/i'.$row['id'].'">'.$row['groupName'].'</a></th>
 				<th>'.$row['adminName'].'</th>
 				<th>'.$members.'</th>
 				<th>'.number_format($row['targetAmount']).' Rwf</th>
