55d0318d1a168938e0518f982d11f27c130b9f7b
Clement <cmuhirwa@users.noreply.github.com>
2018-01-12 02:17:23 +0200

    yes

--- api/test.php
+++ api/test.php
@@ -1,17 +1,48 @@
 <?php 
-$time = time();
-$code = rand(1,1000);
-$newCode = $time."".$code;
-echo "time: ".$time."<br>";
-echo "code: ".$code."<br>";
-echo "newCode: ".$newCode."<br>";
-echo md5($time)."<br>";
-echo md5("1".$time)."<br>";
-echo $str = md5($newCode)."<br>";
-function String2Stars($string='',$first=0,$last=0,$rep='8'){
-  $begin  = substr($string,0,$first);
-  $stars  = $begin;
-  return $stars;
+require('db.php');
+$sentMessage = "testmessage";
+$userId = "1";
+
+function send_notification ($tokens, $message)
+{
+	$url = 'https://fcm.googleapis.com/fcm/send';
+	$fields = array(
+		 'registration_ids' => $tokens,
+		 'data' => $message
+		);
+
+	$headers = array(
+		'Authorization:key = AIzaSyCVsbSeN2qkfDfYq-IwKrnt05M1uDuJxjg',
+		'Content-Type: application/json'
+		);
+
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, $url);
+   curl_setopt($ch, CURLOPT_POST, true);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);  
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+   $result = curl_exec($ch);           
+   if ($result === FALSE) {
+       die('Curl failed: ' . curl_error($ch));
+   }
+   curl_close($ch);
+   return $result;
 }
-echo "Best Code: ".$str2 = String2Stars($str, 4, -4);
+
+$sql = $db->query("SELECT token FROM users WHERE id = '$userId' AND token IS NOT NULL");
+
+$tokens = array();
+
+if(mysqli_num_rows($sql) > 0 ){
+	while ($row = mysqli_fetch_assoc($sql)) {
+		$tokens[] = $row["token"];
+	}
+}
+$message = array("message" => $_POST['message']);
+send_notification($tokens, $message);
+header('Content-Type: application/json');
+			echo $message_status;
 ?>
