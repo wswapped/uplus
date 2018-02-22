0f0b270e4afe588a18a9c0fa9c50d3eb49c68bd1
Clement <muhirwaclement@gmail.com>
2018-02-22 20:48:41 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1542,14 +1542,14 @@
 
 		set_time_limit(0);
 		//File to save the contents to
-		$fp = fopen (dirname(__FILE__) . 'qrcodes/'.$ticketCode.'.png', 'w+');
+		$fp = fopen (dirname(__FILE__) . '/qrcodes/1'.$ticketCode.'.png', 'w+');
 		$url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=".$ticketCode."";
 		//Here is the file we are downloading, replace spaces with %20
 		$ch = curl_init(str_replace(" ","%20",$url));
 		curl_setopt($ch, CURLOPT_TIMEOUT, 500); 
 		//give curl the file pointer so that it can write to it
 		curl_setopt($ch, CURLOPT_FILE, $fp);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		$data = curl_exec($ch);//get curl response
 		//done
 		curl_close($ch);
