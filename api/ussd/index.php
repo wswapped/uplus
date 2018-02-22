87b1104e5a7ecae5f101775baad610e415edf55a
Clement <muhirwaclement@gmail.com>
2018-02-22 14:41:03 +0200

    Update index.php

--- api/ussd/index.php
+++ api/ussd/index.php
@@ -1,5 +1,5 @@
 <?php
-$db = new mysqli("localhost", "clement", "clement123" , "uplus");
+$db = $conn = new mysqli("localhost", "clement", "clement123" , "uplus");
 include_once 'functions.php';
 header("Content-Type: text/plain");
 session_start(); //For web testing only
