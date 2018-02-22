34725f5af90f755bda6d593b111c0d1acb9e1dce
Clement <muhirwaclement@gmail.com>
2018-02-22 15:26:41 +0200

    Update functions.php

--- climate/functions.php
+++ climate/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-	$db = new mysqli("localhost", "clement", "clement123" , "climate");
+	$db = $conn = new mysqli("localhost", "clement", "clement123" , "climate");
 	function get_crops($system)
 	{
 		global $conn;
