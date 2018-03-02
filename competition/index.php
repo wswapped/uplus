bcd4322ccea46d084541806cd7654592b9bbcc00
gokigaligo <clementmuhirwa@gmail.com>
2018-03-02 13:41:53 +0200

    competiton

---
+++ competition/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Uplus win!!</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="container">
+  	<div class="row">
+  		<div class="col-md-7">
+  			<h1>Group creation and members</h1>
+  			<table class="table">
+				  <thead class="thead-active">
+				    <tr>
+				      <th>#</th>
+				      <th>Group name</th>
+				      <th>Created</th>
+				      <th>Members</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <th scope="row">1</th>
+				      <td>Mark</td>
+				      <td>Otto</td>
+				      <td>@mdo</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Jacob</td>
+				      <td>Thornton</td>
+				      <td>@fat</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">3</th>
+				      <td>Larry</td>
+				      <td>the Bird</td>
+				      <td>@twitter</td>
+				    </tr>
+				  </tbody>
+				</table>
+  		</div>
+  		<div class="col-md-5">
+  			<h1>Voting</h1>
+  			<table class="table">
+				  <thead class="thead-active">
+				    <tr>
+				      <th>#</th>
+				      <th>Group name</th>
+				      <th>Created</th>
+				      <th>Voted Members</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <th scope="row">1</th>
+				      <td>Mark</td>
+				      <td>Otto</td>
+				      <td>@mdo</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Jacob</td>
+				      <td>Thornton</td>
+				      <td>@fat</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">3</th>
+				      <td>Larry</td>
+				      <td>the Bird</td>
+				      <td>@twitter</td>
+				    </tr>
+				  </tbody>
+				</table>
+  		</div>
+  	</div>
+  </div>
+<script type="text/javascript" src="../assets/js/jquery.slim.min.js"></script>
+<script type="text/javascript" src="../assets/js/tether.min.js"></script>
+<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../assets/js/js.js"></script>
+</body>
+</html>
